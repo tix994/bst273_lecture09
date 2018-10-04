ccf3b89e3387d8ff67c47c9f7e012401819f286e
tix994 <txia@hsph.harvard.edu>
2018-10-04 12:28:13 -0400

    open and read lines of files

--- word_count.py
+++ word_count.py
@@ -19,9 +19,31 @@ parser.add_argument(
 
 args = parser.parse_args( )
 
+# Set path
 print( args )
-print( args.data_file )
+print( "args.data_file = ", args.data_file )
+
+# open file
+# open( "data_file" ) # open a file name called data_file
+fh = open( args.data_file ) # need to store it in a variabels
+print( "the file handle is", fh )
+
+# the output are 3 numbers
+lines = 0
+words = 0
+chars = 0 # bytes is a built in one
+
+for line in fh:
+    print(line) # but print a giant output
 
 #-------------------------------------------------------------------------------
 # our code for analyzing the data
 #-------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
